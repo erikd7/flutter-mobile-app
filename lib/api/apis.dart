ecbee01d9a3bf4dacad1441991b02a515eeefdff
Erik <eb.dietrich11@gmail.com>
2022-01-27 22:10:14 -0600

    Http call to weather API and basic display

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,16 +1,24 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class WeatherApis {
+import 'package:weatherlite/models.dart';
+
+class WeatherApi {
   var client = http.Client();
   String base = 'https://api.openweathermap.org/data/2.5/';
   String apikey = dotenv.env['WEATHER_API_KEY'] ?? 'no_api_key';
   String location;
   String units;
-  WeatherApis(this.location, this.units);
+  WeatherApi(this.location, this.units);
 
-  void getCurrentWeather() {
-    final Future<http.Response> x = client.get(Uri.parse(
-        '${base}weather?q=${location}&appid=${apikey}&units=${units}'));
+  Future<Weather> getCurrentWeather() async {
+    final response = await client.get(
+        Uri.parse(
+            '${base}weather?q=${location}&appid=${apikey}&units=${units}'),
+        headers: {"Accept": "application/json"});
+
+    var responseData = json.decode(response.body);
+    return Weather.fromJson(responseData);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,35 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WeatherLite',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Now'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -59,7 +39,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late TabController controller = TabController(length: 4, vsync: this);
-  final WeatherApis apis = WeatherApis('madison,wi,usa', 'imperial');
+  final WeatherApi apis = WeatherApi('madison,wi,usa', 'imperial');
 
   TabBar getTabBar() {
     return TabBar(
@@ -97,9 +77,8 @@ class _MyHomePageState extends State<MyHomePage>
       appBar: AppBar(
         title: getTabBar(),
       ),
-      body: getTabBarView(<Widget>[Locations(), Now(), Hourly(), Daily()]),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () => apis.getCurrentWeather()),
+      body: getTabBarView(
+          <Widget>[Locations(), Now(apis: apis), Hourly(), Daily()]),
     );
   }
 }
